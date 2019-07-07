1b60cb856167498dbbc4db79e22110c1fe668ea0
ollyroberts <olly.roberts001@gmail.com>
2019-07-07 15:35:43 +0100

    Modifying refactored code

--- refactored_code/refactored_code/main.py
+++ refactored_code/refactored_code/main.py
@@ -1,4 +1,6 @@
 import subprocess
-print('Start Main')
-subprocess.call(['python', 'onehelixres.py', '1ct5.sec', '1ct5.1hr'])
+print('Start Main onehelixres_refactored')
+subprocess.call(['python', 'onehelixres_refactored.py', '../existing_code/1ct5.sec', '../existing_code/1ct5.1hr'])
+print('Start Main twohelixres')
+subprocess.call(['python', 'twohelixres_refactored.py', '../existing_code/1ct5.sec', '../existing_code/1h3l.2hr'])
 print('Finish Main')
